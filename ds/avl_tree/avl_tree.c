f6cbd0f3c59ec0d02779adf85a03d7a3b4154795
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-10-11 16:07:29 -0700

    Test AVL Tree implementation

--- ds/avl_tree/avl_tree.c
+++ ds/avl_tree/avl_tree.c
@@ -1,9 +1,22 @@
 #include "avl_tree.h"
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /* Helper functions */
+
+static Node_t* init_node(int key)
+{
+    Node_t* node = calloc(1, sizeof(Node_t));
+    if (node == NULL)
+    {
+        fprintf(stderr, "Error: calloc failed in init_node\n");
+        exit(EXIT_FAILURE);
+    }
+    node->key = key;
+    node->height = 0;
+    node->size = 1;
+    return node;
+}
 
 static void swap_keys(Node_t* node1, Node_t* node2)
 {
@@ -24,12 +37,13 @@ static Node_t* predecessor(Node_t* node)
         fprintf(stderr, "Error: node is NULL in predecessor\n");
         exit(EXIT_FAILURE);
     }
-    // Case: Left child exists
+    // Case: Left child exists; predecessor is lower in the tree
     if (node->left != NULL)
     {
         return find_max_avl_tree(&node->left);
     }
-    // Case: Left child does not exist
+    // Case: Left child does not exist; predecessor is higher in the tree (if it
+    // exists)
     else
     {
         Node_t* parent = node->parent;
@@ -56,12 +70,13 @@ static Node_t* successor(Node_t* node)
         fprintf(stderr, "Error: node is NULL in successor\n");
         exit(EXIT_FAILURE);
     }
-    // Case: Right child exists
+    // Case: Right child exists; successor is lower in the tree
     if (node->right != NULL)
     {
         return find_min_avl_tree(&node->right);
     }
-    // Case: Right child does not exist
+    // Case: Right child does not exist; successor is higher in the tree (if
+    // it exists)
     else
     {
         Node_t* parent = node->parent;
@@ -81,7 +96,23 @@ static Node_t* successor(Node_t* node)
     }
 }
 
-static void rotate_right(Node_t* node)
+static void update_subtree_augmentations(Node_t* node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    node->height = (node->left == NULL && node->right == NULL)
+                       ? 0
+                       : 1 + MAX(node->left == NULL ? 0 : node->left->height,
+                                 node->right == NULL ? 0 : node->right->height);
+    node->size = 1 + (node->left == NULL ? 0 : node->left->size) +
+                 (node->right == NULL ? 0 : node->right->size);
+    // Recursively update ancestors' subtree augmentations
+    update_subtree_augmentations(node->parent);
+}
+
+static AVLTree_t rotate_right(AVLTree_t root, Node_t* node)
 {
     if (node == NULL)
     {
@@ -91,60 +122,58 @@ static void rotate_right(Node_t* node)
     // Right rotation is noop when left child is NULL
     if (node->left == NULL)
     {
-        return;
+        return root;
     }
     else
     {
-        Node_t* new_root = node->left;
+        Node_t* new_subtree_root = node->left;
 
         // New root's parent becomes old root's parent
-        new_root->parent = node->parent;
+        new_subtree_root->parent = node->parent;
         if (node->parent != NULL)
         {
             // Old root's parent's left child becomes new root if old root was
             // the left child
             if (node->parent->left == node)
             {
-                node->parent->left = new_root;
+                node->parent->left = new_subtree_root;
             }
             // Old root's parent's right child becomes new root if old root was
             // the right child
             else
             {
-                node->parent->right = new_root;
+                node->parent->right = new_subtree_root;
             }
+        }
+        else
+        {
+            *root = new_subtree_root;
         }
 
         // Old root's left child becomes new root's right child
-        node->left = new_root->right;
-        new_root->right->parent = node;
+        node->left = new_subtree_root->right;
+        if (new_subtree_root->right != NULL)
+        {
+            new_subtree_root->right->parent = node;
+        }
 
         // Old root becomes new root's right child
-        node->parent = new_root;
-        new_root->right = node;
+        node->parent = new_subtree_root;
+        new_subtree_root->right = node;
 
         // Recompute subtree augmentations
-        node->height =
-            node->left == NULL && node->right == NULL
-                ? 0
-                : 1 + MAX(node->left == NULL ? 0 : node->left->height,
-                          node->right == NULL ? 0 : node->right->height);
-        new_root->height =
-            new_root->left == NULL && new_root->right == NULL
-                ? 0
-                : 1 + MAX(new_root->left == NULL ? 0 : new_root->left->height,
-                          new_root->right == NULL ? 0
-                                                  : new_root->right->height);
-        node->size = 1 + (node->left == NULL ? 0 : node->left->size) +
-                     (node->right == NULL ? 0 : node->right->size);
-        new_root->size = 1 +
-                         (new_root->left == NULL ? 0 : new_root->left->size) +
-                         (new_root->right == NULL ? 0 : new_root->right->size);
+        update_subtree_augmentations(node);
     }
+    return root;
 }
 
-static void rotate_left(Node_t* node)
+static AVLTree_t rotate_left(AVLTree_t root, Node_t* node)
 {
+    if (root == NULL)
+    {
+        fprintf(stderr, "Error: root is NULL in rotate_left\n");
+        exit(EXIT_FAILURE);
+    }
     if (node == NULL)
     {
         fprintf(stderr, "Error: node is NULL in rotate_left\n");
@@ -153,56 +182,49 @@ static void rotate_left(Node_t* node)
     // Left rotation is noop when right child is NULL
     if (node->right == NULL)
     {
-        return;
+        return root;
     }
     else
     {
-        Node_t* new_root = node->right;
+        Node_t* new_subtree_root = node->right;
 
         // New root's parent becomes old root's parent
-        new_root->parent = node->parent;
+        new_subtree_root->parent = node->parent;
         if (node->parent != NULL)
         {
             // Old root's parent's left child becomes new root if old root was
             // the left child
             if (node->parent->left == node)
             {
-                node->parent->left = new_root;
+                node->parent->left = new_subtree_root;
             }
             // Old root's parent's right child becomes new root if old root was
             // the right child
             else
             {
-                node->parent->right = new_root;
+                node->parent->right = new_subtree_root;
             }
+        }
+        else
+        {
+            *root = new_subtree_root;
         }
 
         // Old root's right child becomes new root's left child
-        node->right = new_root->left;
-        new_root->left->parent = node;
+        node->right = new_subtree_root->left;
+        if (new_subtree_root->left != NULL)
+        {
+            new_subtree_root->left->parent = node;
+        }
 
         // Old root becomes new root's left child
-        node->parent = new_root;
-        new_root->left = node;
+        node->parent = new_subtree_root;
+        new_subtree_root->left = node;
 
         // Recompute subtree augmentations
-        node->height =
-            node->left == NULL && node->right == NULL
-                ? 0
-                : 1 + MAX(node->left == NULL ? 0 : node->left->height,
-                          node->right == NULL ? 0 : node->right->height);
-        new_root->height =
-            new_root->left == NULL && new_root->right == NULL
-                ? 0
-                : 1 + MAX(new_root->left == NULL ? 0 : new_root->left->height,
-                          new_root->right == NULL ? 0
-                                                  : new_root->right->height);
-        node->size = 1 + (node->left == NULL ? 0 : node->left->size) +
-                     (node->right == NULL ? 0 : node->right->size);
-        new_root->size = 1 +
-                         (new_root->left == NULL ? 0 : new_root->left->size) +
-                         (new_root->right == NULL ? 0 : new_root->right->size);
+        update_subtree_augmentations(node);
     }
+    return root;
 }
 
 static int skew(Node_t* node)
@@ -212,15 +234,20 @@ static int skew(Node_t* node)
         fprintf(stderr, "Error: node is NULL in skew\n");
         exit(EXIT_FAILURE);
     }
-    return (node->right == NULL ? 0 : node->right->height) -
-           (node->left == NULL ? 0 : node->left->height);
+    return (node->right == NULL ? -1 : node->right->height) -
+           (node->left == NULL ? -1 : node->left->height);
 }
 
-static void rebalance_at_node(Node_t* node)
+static AVLTree_t rebalance_at_node(AVLTree_t root, Node_t* node)
 {
+    if (root == NULL)
+    {
+        fprintf(stderr, "Error: root is NULL in rebalance_at_node\n");
+        exit(EXIT_FAILURE);
+    }
     if (node == NULL)
     {
-        return;
+        return root;
     }
     int skewness = skew(node);
     // Case: Right-heavy subtree
@@ -229,9 +256,9 @@ static void rebalance_at_node(Node_t* node)
         // Subcase: Right-left-heavy subtree two rotations
         if (skew(node->right) == -1)
         {
-            rotate_right(node->right);
+            root = rotate_right(root, node->right);
         }
-        rotate_left(node);
+        root = rotate_left(root, node);
     }
     // Case: Left-heavy subtree
     else if (skewness == -2)
@@ -239,23 +266,12 @@ static void rebalance_at_node(Node_t* node)
         // Subcase: Left-right-heavy subtree requires two rotations
         if (skew(node->left) == 1)
         {
-            rotate_left(node->left);
+            root = rotate_left(root, node->left);
         }
-        rotate_right(node);
-    }
-    // Case: Subtree is balanced, but subtree augmentations must be updated
-    else
-    {
-        node->height =
-            (node->left == NULL && node->right == NULL)
-                ? 0
-                : 1 + MAX(node->left == NULL ? 0 : node->left->height,
-                          node->right == NULL ? 0 : node->right->height);
-        node->size = 1 + (node->left == NULL ? 0 : node->left->size) +
-                     (node->right == NULL ? 0 : node->right->size);
+        root = rotate_right(root, node);
     }
     // Recursively rebalance ancestors
-    rebalance_at_node(node->parent);
+    return rebalance_at_node(root, node->parent);
 }
 
 static void free_subtree_at_node(Node_t* node)
@@ -283,16 +299,20 @@ AVLTree_t init_avl_tree(int data[], size_t size)
         fprintf(stderr, "Error: data is NULL in init_avl_tree\n");
         exit(EXIT_FAILURE);
     }
+
     AVLTree_t root = malloc(sizeof(AVLTree_t));
     if (root == NULL)
     {
         fprintf(stderr, "Error: malloc failed in init_avl_tree\n");
         exit(EXIT_FAILURE);
     }
+
+    *root = NULL;
     for (size_t i = 0; i < size; i++)
     {
         insert_avl_tree(root, data[i]);
     }
+
     return root;
 }
 
@@ -309,84 +329,86 @@ void insert_avl_tree(AVLTree_t root, int key)
         return;
     }
 
-    // Create new node
-    Node_t* new_node = calloc(1, sizeof(Node_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: calloc failed in insert_avl_tree\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->key = key;
-    new_node->size = 1;
+    Node_t* node = *root;
 
     // Case: Empty tree
-    if (*root == NULL)
+    if (node == NULL)
     {
+        Node_t* new_node = init_node(key);
         *root = new_node;
+        return;
     }
     // Case: Non-empty tree
-    else
+    do
     {
-        Node_t* node = *root;
-        if (new_node->key < node->key)
+        if (key < node->key)
         {
             if (node->left == NULL)
             {
+                Node_t* new_node = init_node(key);
                 node->left = new_node;
                 new_node->parent = node;
-                rebalance_at_node(node);
+                update_subtree_augmentations(new_node);
+                *root = *rebalance_at_node(root, new_node);
+                return;
             }
             else
             {
-                insert_avl_tree(&node->left, key);
+                node = node->left;
             }
         }
         else
         {
             if (node->right == NULL)
             {
+                Node_t* new_node = init_node(key);
                 node->right = new_node;
                 new_node->parent = node;
-                rebalance_at_node(node);
+                update_subtree_augmentations(new_node);
+                *root = *rebalance_at_node(root, new_node);
+                return;
             }
             else
             {
-                insert_avl_tree(&node->right, key);
+                node = node->right;
             }
         }
-    }
+    } while (true);
 }
 
 void delete_avl_tree(AVLTree_t root, int key)
 {
-    Node_t* node = find_avl_tree(root, key);
-    if (node == NULL)
+    if (root == NULL)
+    {
+        fprintf(stderr, "Error: root is NULL in find_avl_tree\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!query_avl_tree(root, key))
     {
         fprintf(stdout, "Warning: key %i not found in tree\n", key);
         return;
     }
-
-    int node_key = node->key;
-
-    // Case: Node has a child; swap with predecessor or successor
-    if (node->left != NULL || node->right != NULL)
+    // Case: Node has a child (is not a leaf)
+    Node_t* node = find_avl_tree(root, key);
+    while (node->left != NULL || node->right != NULL)
     {
         if (node->left != NULL)
         {
-            Node_t* predecessor = find_prev_avl_tree(root, node_key);
-            swap_keys(node, predecessor);
-            delete_avl_tree(&node->left, node_key);
+            Node_t* prev = predecessor(node);
+            swap_keys(node, prev);
+            node = prev;
         }
         else
         {
-            Node_t* successor = find_next_avl_tree(root, node_key);
-            swap_keys(node, successor);
-            delete_avl_tree(&node->right, node_key);
+            Node_t* next = successor(node);
+            swap_keys(node, next);
+            node = next;
         }
     }
     // Case: Node is non-root leaf; no swapping required, but parent
     // pointers must be updated
-    else if (node->parent != NULL)
+    if (node->parent != NULL)
     {
         if (node->parent->left == node)
         {
@@ -396,14 +418,15 @@ void delete_avl_tree(AVLTree_t root, int key)
         {
             node->parent->right = NULL;
         }
-        rebalance_at_node(node->parent);
+        update_subtree_augmentations(node->parent);
+        *root = *rebalance_at_node(root, node->parent);
         free_subtree_at_node(node);
     }
     // Case: Node is root leaf
     else
     {
-        free_subtree_at_node(node);
         *root = NULL;
+        free_subtree_at_node(node);
     }
 }
 
@@ -414,7 +437,9 @@ Node_t* find_avl_tree(AVLTree_t root, int key)
         fprintf(stderr, "Error: root is NULL in find_avl_tree\n");
         exit(EXIT_FAILURE);
     }
+
     Node_t* node = *root;
+
     // Case: Empty tree
     if (node == NULL)
     {
@@ -459,35 +484,33 @@ Node_t* find_prev_avl_tree(AVLTree_t root, int key)
         fprintf(stderr, "Error: root is NULL in find_prev_avl_tree\n");
         exit(EXIT_FAILURE);
     }
-    Node_t* node = *root;
-    // Case: Empty tree
-    if (node == NULL)
+    if (query_avl_tree(root, key))
+    {
+        return predecessor(find_avl_tree(root, key));
+    }
+    Node_t *min_node, *max_node;
+    if (key < (min_node = find_min_avl_tree(root))->key ||
+        is_empty_avl_tree(root))
     {
         return NULL;
     }
-    // Case: Non-empty tree
-    Node_t* parent = node->parent;
-    while (node != NULL && node->key != key)
+    if (key > (max_node = find_max_avl_tree(root))->key)
     {
-        // Subcase: Recurse left
+        return max_node;
+    }
+    Node_t* node = *root;
+    while (node->left != NULL || node->right != NULL)
+    {
         if (key < node->key)
         {
-            parent = node;
             node = node->left;
         }
-        // Subcase: Recurse right
         else
         {
-            parent = node;
             node = node->right;
         }
-        // Case: Key not found
-        if (node == NULL)
-        {
-            node = parent;
-        }
     }
-    return predecessor(node);
+    return (key > node->key) ? node : successor(node);
 }
 
 Node_t* find_next_avl_tree(AVLTree_t root, int key)
@@ -497,35 +520,33 @@ Node_t* find_next_avl_tree(AVLTree_t root, int key)
         fprintf(stderr, "Error: root is NULL in find_next_avl_tree\n");
         exit(EXIT_FAILURE);
     }
-    Node_t* node = *root;
-    // Case: Empty tree
-    if (node == NULL)
+    if (query_avl_tree(root, key))
+    {
+        return successor(find_avl_tree(root, key));
+    }
+    Node_t *min_node, *max_node;
+    if (key > (max_node = find_max_avl_tree(root))->key ||
+        is_empty_avl_tree(root))
     {
         return NULL;
     }
-    // Case: Non-empty tree
-    Node_t* parent = node->parent;
-    while (node != NULL && node->key != key)
+    if (key < (min_node = find_min_avl_tree(root))->key)
     {
-        // Subcase: Recurse left
+        return min_node;
+    }
+    Node_t* node = *root;
+    while (node->left != NULL || node->right != NULL)
+    {
         if (key < node->key)
         {
-            parent = node;
             node = node->left;
         }
-        // Subcase: Recurse right
         else
         {
-            parent = node;
             node = node->right;
         }
-        // Case: Key not found
-        if (node == NULL)
-        {
-            node = parent;
-        }
     }
-    return successor(node);
+    return (key < node->key) ? node : successor(node);
 }
 
 Node_t* find_min_avl_tree(AVLTree_t root)
@@ -589,4 +610,5 @@ void free_avl_tree(AVLTree_t root)
     {
         free_subtree_at_node(*root);
     }
+    free(root);
 }
