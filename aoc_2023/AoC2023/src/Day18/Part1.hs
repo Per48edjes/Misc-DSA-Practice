ec5e512d13b6c63f292d5a0f2daf5894882a9b9b
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-12-25 13:05:36 -0500

    Implemented indexed coordinate approach

--- aoc_2023/AoC2023/src/Day18/Part1.hs
+++ aoc_2023/AoC2023/src/Day18/Part1.hs
@@ -2,8 +2,6 @@
 
 module Day18.Part1 (solution, totalTrenchVolume) where
 
-import Debug.Trace (trace)
-
 import Control.Monad.State.Strict
 import Data.Attoparsec.Text
 import Data.Function (on)
@@ -15,12 +13,13 @@ import qualified Data.Set as S
 import Data.Text (Text)
 import qualified Data.Text.IO as TIO
 import Day10.Part1 (Coord, Direction (..))
+import Day11.Part1 (Index)
 import Day16.Part1 (vectorDir)
 
 -- Types
 
 type PlanEntry = (Direction, Int, Text)
-type DigState = State (Coord, Set Coord) ()
+type DigState = State (Coord, Set (Index, Coord)) ()
 
 -- Functions
 
@@ -35,45 +34,47 @@ solution filePath valueFunc = do
 totalTrenchVolume :: [PlanEntry] -> Int
 totalTrenchVolume = calculateTrenchInterior . getTrench
 
-calculateTrenchInterior :: Set Coord -> Int
+calculateTrenchInterior :: Set (Index, Coord) -> Int
 calculateTrenchInterior trench =
     let ((minX, maxX), (minY, maxY)) = getBoundingDimensions trench
-        trenchCoords = S.toList trench
-        candidateCoords = [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY], (x, y) `S.notMember` trench]
-     in S.size . S.fromList $ trenchCoords ++ filter (raycastLeft trench) candidateCoords
+        candidateCoords = [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY], (x, y) `S.notMember` S.map snd trench]
+     in S.size trench + length (filter (raycastLeft trench) candidateCoords)
 
-getTrench :: [PlanEntry] -> Set Coord
+getTrench :: [PlanEntry] -> Set (Index, Coord)
 getTrench plan =
-    let (_, visited) = execState (mapM_ dig plan) ((0, 0), S.empty)
+    let (_, visited) = execState (mapM_ dig idxPlan) ((0, 0), S.empty)
      in visited
+  where
+    idxPlan = zip [0 ..] plan
 
 -- | Digs the trench according to the plan
-dig :: PlanEntry -> DigState
-dig (dir, steps, _) = do
+dig :: (Index, PlanEntry) -> DigState
+dig (i, (dir, steps, _)) = do
     (curCoord, visited) <- get
     let (x', y') = vectorDir dir
-        newCoords = L.take (steps + 1) $ iterate (\(x, y) -> (x + x', y + y')) curCoord
+        newCoords = fmap (i,) $ tail $ L.take (steps + 1) $ iterate (\(x, y) -> (x + x', y + y')) curCoord
         newVisited = S.union visited (S.fromList newCoords)
-    put (last newCoords, newVisited)
+    put (snd . last $ newCoords, newVisited)
 
 -- | Casts a ray left of the input Coord and returns True if the number of intersections with the trench is odd
-raycastLeft :: Set Coord -> Coord -> Bool
+raycastLeft :: Set (Index, Coord) -> Coord -> Bool
 raycastLeft trench coord = odd $ length groupedTrenchCoords
   where
     ((_, _), (minY, _)) = getBoundingDimensions trench
     (x', y') = vectorDir W
-    trenchCoords = filter (`S.member` trench) $ L.take (abs (minY - snd coord) + 1) $ iterate (\(x, y) -> (x + x', y + y')) coord
-    groupedTrenchCoords = groupByConsecutiveY trenchCoords
+    ray = S.fromList $ L.take (abs (minY - snd coord) + 1) $ iterate (\(x, y) -> (x + x', y + y')) coord
+    trenchCoords = S.toList $ S.filter (\(_, c) -> c `S.member` ray) trench
+    groupedTrenchCoords = groupTrenchWalls trenchCoords
 
--- | Function to sort Coords and group by consecutive y-coordinates
-groupByConsecutiveY :: [(Int, Int)] -> [[(Int, Int)]]
-groupByConsecutiveY = G.groupBy (\a b -> succ (snd a) == snd b) . L.sortBy (compare `on` snd)
+-- | Function to sort Coords and group by consecutive y-coordinates and instruction index
+groupTrenchWalls :: [(Index, Coord)] -> [[(Index, Coord)]]
+groupTrenchWalls = G.groupBy (\(i1, (_, y1)) (i2, (_, y2)) -> (abs (y1 - y2) == 1) && (i1 == i2 || abs (i1 - i2) == 1)) . L.sortBy (compare `on` snd . snd)
 
 -- | Gets a the bounding coordinates a trench
-getBoundingDimensions :: Set Coord -> ((Int, Int), (Int, Int))
+getBoundingDimensions :: Set (Index, Coord) -> ((Int, Int), (Int, Int))
 getBoundingDimensions coords =
-    let xs = S.map fst coords
-        ys = S.map snd coords
+    let xs = S.map fst $ S.map snd coords
+        ys = S.map snd $ S.map snd coords
      in ((S.findMin xs, S.findMax xs), (S.findMin ys, S.findMax ys))
 
 -- Parsers
