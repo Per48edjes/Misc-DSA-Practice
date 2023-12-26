81e22040d312593de522b67149066a0f8920d39d
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-12-26 16:14:18 -0500

    Try flood-fill approach

--- aoc_2023/AoC2023/src/Day18/Part1.hs
+++ aoc_2023/AoC2023/src/Day18/Part1.hs
@@ -2,14 +2,12 @@
 
 module Day18.Part1 (solution, totalTrenchVolume) where
 
-import Debug.Trace (trace)
-
 import Control.Monad.State.Strict
 import Data.Attoparsec.Text
-import Data.Function (on)
+import Data.Bifunctor (bimap)
 import Data.Functor (($>))
+import Data.Ix (Ix (inRange))
 import qualified Data.List as L
-import qualified Data.List.GroupBy as G
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Text (Text)
@@ -23,6 +21,8 @@ import Day16.Part1 (vectorDir)
 type PlanEntry = (Direction, Int, Text)
 type DigState = State (Coord, Set (Index, Coord)) ()
 
+type FillState = State (Set Coord) ()
+
 -- Functions
 
 solution :: FilePath -> ([PlanEntry] -> Int) -> IO Int
@@ -34,7 +34,7 @@ solution filePath valueFunc = do
     return $ valueFunc digPlan
 
 totalTrenchVolume :: [PlanEntry] -> Int
-totalTrenchVolume plan = (S.size . getTrenchCoords . getTrench $ plan) + (calculateTrenchInterior . getTrench $ plan)
+totalTrenchVolume = calculateTrenchInterior . getTrench
 
 calculateTrenchInterior :: Set (Index, Coord) -> Int
 calculateTrenchInterior trench = S.size (getInteriorCoords trench)
@@ -49,11 +49,13 @@ getTrench plan =
 getTrenchCoords :: Set (Index, Coord) -> Set Coord
 getTrenchCoords = S.map snd
 
+-- | Uses flood fill of exterior to impute interior coordinate set
 getInteriorCoords :: Set (Index, Coord) -> Set Coord
 getInteriorCoords trench =
     let ((minX, maxX), (minY, maxY)) = getBoundingDimensions trench
-        candidateCoords = [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY], (x, y) `S.notMember` getTrenchCoords trench]
-     in S.fromList $ filter (raycastLeft trench) candidateCoords
+        ((minX', maxX'), (minY', maxY')) = ((minX - 1, maxX + 1), (minY - 1, maxY + 1))
+        candidateCoords = S.fromList [(x, y) | x <- [minX' .. maxX'], y <- [minY' .. maxY']]
+     in candidateCoords S.\\ getExteriorCoords trench
 
 dig :: (Index, PlanEntry) -> DigState
 dig (i, (dir, steps, _)) = do
@@ -63,40 +65,31 @@ dig (i, (dir, steps, _)) = do
         newVisited = S.union visited (S.fromList newCoords)
     put (snd . last $ newCoords, newVisited)
 
--- | Point-in-a-polygon algorithm that casts ray from right (leftward) and counts the number of times it enters and leaves polygon formed by the trench walls
-raycastLeft :: Set (Index, Coord) -> Coord -> Bool
-raycastLeft trench coord@(x, _) =
-    let
-        ((_, _), (_, maxY)) = getBoundingDimensions trench
-        (x', y') = vectorDir W
-        ray = S.fromList $ L.takeWhile (/= coord) $ iterate (\c -> (fst c + x', snd c + y')) (x, maxY + 1)
-        trenchCoords = S.toList $ S.filter (\(_, c) -> c `S.member` ray) trench
-        groupedTrenchCoords = groupTrenchWalls trenchCoords
-     in
-        (odd . sum . determineGroupConcavity) groupedTrenchCoords
+getExteriorCoords :: Set (Index, Coord) -> Set Coord
+getExteriorCoords trench = execState (floodFillTrench trench' augmentedBounds (minX', minY')) S.empty
   where
-    determineGroupConcavity :: [[(Index, Coord)]] -> [Int]
-    determineGroupConcavity groups = do
-        group <- groups
-        let (minGroupY, maxGroupY) = (snd $ snd (head group), snd $ snd (last group))
-            isConvex =
-                S.member (x - 1, minGroupY) trench'
-                    && S.member (x + 1, maxGroupY) trench'
-                    || S.member (x + 1, minGroupY) trench'
-                        && S.member (x - 1, maxGroupY) trench'
-        return $ if isConvex then 1 else 0
-      where
-        trench' = getTrenchCoords trench
+    ((minX, maxX), (minY, maxY)) = getBoundingDimensions trench
+    augmentedBounds@((minX', _), (minY', _)) = ((minX - 1, maxX + 1), (minY - 1, maxY + 1))
+    trench' = getTrenchCoords trench
 
--- | Groups together trench walls that are adjacent to each other
-groupTrenchWalls :: [(Index, Coord)] -> [[(Index, Coord)]]
-groupTrenchWalls = G.groupBy (\(i1, (_, y1)) (i2, (_, y2)) -> abs (y1 - y2) == 1 && (i1 == i2 || abs (i1 - i2) == 1)) . L.sortBy (compare `on` snd . snd)
+floodFillTrench :: Set Coord -> ((Index, Index), (Index, Index)) -> Coord -> FillState
+floodFillTrench trench' augmentedBounds@((minX', maxX'), (minY', maxY')) curCoord = do
+    visited <- get
+    unless (curCoord `S.member` visited) $ do
+        let neighbors = getNeighbors curCoord
+            unvisitedNeighbors = filter (`S.notMember` visited) neighbors
+        put (S.insert curCoord visited)
+        mapM_ (floodFillTrench trench' augmentedBounds) unvisitedNeighbors
+  where
+    getNeighbors (x, y) =
+        let allNeighbors = [bimap (+ x) (+ y) $ vectorDir dir | dir <- [N, S, E, W]]
+         in filter (\c -> c `S.notMember` trench' && inRange ((minX', minY'), (maxX', maxY')) c) allNeighbors
 
 -- | Gets a the bounding coordinates a trench
 getBoundingDimensions :: Set (Index, Coord) -> ((Int, Int), (Int, Int))
-getBoundingDimensions coords =
-    let xs = S.map fst $ S.map snd coords
-        ys = S.map snd $ S.map snd coords
+getBoundingDimensions trench =
+    let xs = S.map fst $ getTrenchCoords trench
+        ys = S.map snd $ getTrenchCoords trench
      in ((S.findMin xs, S.findMax xs), (S.findMin ys, S.findMax ys))
 
 -- Parsers
